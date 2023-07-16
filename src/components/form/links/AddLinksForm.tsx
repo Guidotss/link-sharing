96c34b1c6e5372b620355a6c9239ce8011295405
Guidotss <guidoolguin@hotmail.com>
2023-07-15 22:21:53 -0300

    Emepezando componente AddLinkForm

---
+++ src/components/form/links/AddLinksForm.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useContext } from "react"
+import { LinksContext } from "@/context"
+
+export const AddLinksForm = () => {
+    const { links } = useContext(LinksContext); 
+  return (
+    <form>
+
+    </form>
+  )
+}
