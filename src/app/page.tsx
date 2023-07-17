69879d45315b31531f34b86fb9f0c5d232f7c4b9
Guidotss <guidoolguin@hotmail.com>
2023-07-17 20:36:53 -0300

    Arreglando tamaños en notebook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="px-20 py-5 bg-light_grey">
       <Navbar />
-      <section className="flex 2xl:gap-3 gap-40 px-20 mt-14">
-        <div className="bg-white flex-auto h-[75vh] w-full flex items-center justify-center rounded-lg">
+      <section className="flex 2xl:gap-3 px-20 mt-14">
+        <div className="bg-white flex-auto 2xl:h-[75vh] h-full w-full flex items-center justify-center rounded-lg">
           <MockUpIcon />
         </div>
         <div className="w-full bg-white min-h-[75vh] items-center justify-center rounded-lg p-10">
--- src/components/ui/links/EmptySection.tsx
+++ src/components/ui/links/EmptySection.tsx
@@ -5,7 +5,7 @@ export const EmptySection = () => {
     <div className="flex flex-col items-center justify-center mt-24">
       <IllustrationEmptyIcon />
       <h3 className="text-4xl font-bold mt-10">Let{"'"}s get you started</h3>
-      <p className="2xl:w-[55%] w-[90%] mt-10 text-grey text-center">
+      <p className="2xl:w-[55%] mt-10 text-grey text-center">
         Use the “Add new link” button to get started. Once you have more than
         one link, you can reorder and edit them. We’re here to help you share
         your profiles with everyone!
