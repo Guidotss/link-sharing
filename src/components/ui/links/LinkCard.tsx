8aba898649bca9d2340975f6da7ae8ee574b68a3
Guidotss <guidoolguin@hotmail.com>
2023-07-19 16:48:50 -0300

    Terminando copia en porta papeles

--- src/components/ui/links/LinkCard.tsx
+++ src/components/ui/links/LinkCard.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { selectOptions } from "@/constants";
 import { Links } from "@/interfaces";
 import { RightArrowIcon } from "../icons";
+import "animate.css"; 
 
 
 interface LinkCardProps {
@@ -10,12 +11,22 @@ interface LinkCardProps {
 }
 
 export const LinkCard: FC<LinkCardProps> = ({ link }) => {
+  const [ isCopied, setIsCopied ] = useState(false);
   let bgColor = selectOptions.find(
     (option) => option.name.toLowerCase() === link.name.toLowerCase()
   )?.bgColor;
   if (bgColor == "white") {
     bgColor = `bg-${bgColor}`;
   }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(link.url); 
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    },1000); 
+  } 
 
 
 
@@ -33,9 +44,18 @@ export const LinkCard: FC<LinkCardProps> = ({ link }) => {
           {link.name}
         </h1>
       </div>
-      <div className="mr-2 2xl:mr-4">
-        <RightArrowIcon isWhite={bgColor == "bg-white" ? false : true} />
+      <div className="mr-2 2xl:mr-4 cursor-pointer" onClick={handleCopy}>
+        {isCopied 
+          ? ( 
+              <span className="absolute right-3 -mt-3 animate__animated animate__fadeInDown">Copied</span>
+            )
+            : ( 
+              <RightArrowIcon isWhite={bgColor == "bg-white" ? false : true} />
+            )
+        }
       </div>
+      
     </div>
+    
   );
 };
