f9bcc22061b240e5a6ceec81c91361b25e0c44c6
Guidotss <guidoolguin@hotmail.com>
2023-07-19 21:20:51 -0300

    merge files

--- src/components/ui/links/LinkCard.tsx
+++ src/components/ui/links/LinkCard.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { selectOptions } from "@/constants";
 import { Links } from "@/interfaces";
 import { RightArrowIcon } from "../icons";
-import { setBackgroundColor } from "@/helpers";
 
 interface LinkCardProps {
   link: Links;
@@ -11,13 +10,12 @@ interface LinkCardProps {
 
 export const LinkCard: FC<LinkCardProps> = ({ link }) => {
   const [bgColor, setBgColor] = useState("");
-  useEffect(() => {
-    setBgColor(setBackgroundColor(link.name.toLowerCase()));
-  }, [link.name]);
+  
+
 
   return (
     <div
-      className={` bg-${bgColor} rounded-lg flex justify-between items-center py-3 text-white`}
+      className={`rounded-lg flex justify-between items-center py-3 text-white`}
     >
       <div className="flex items-center gap-2 ml-2 ">
         {selectOptions
