ef29bf81dcabcc769258b36f2986bf90c721b101
Guidotss <guidoolguin@hotmail.com>
2023-07-27 15:03:22 -0300

    Ajustando responsive mobile RegisterForm

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from "@/components/form";
 const LoginPage = async () => {
   return (
     <main className="flex flex-col items-center mt-4">
-      <div className="flex mb-10 mt-5">
+      <div className="flex 2xl:mb-10 md:mb-0 mb-10  mt-5">
         <DevlinksLargeIcon />
       </div>
       <LoginForm />
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -4,7 +4,7 @@ import { RegisterForm } from "@/components/form";
 const RegisterPage = () => {
   return (
     <main className="flex flex-col items-center 2xl:justify-center h-screen py-10 md:py-0">
-      <div className="flex 2xl:mb-10">
+      <div className="flex 2xl:mb-10 mb-10 md:mb-0">
         <DevlinksLargeIcon />
       </div>
       <RegisterForm />
