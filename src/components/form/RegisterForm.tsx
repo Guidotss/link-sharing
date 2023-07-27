df91f2b1c4c60b4e54efefd014e1ed7b2190adc9
Guidotss <guidoolguin@hotmail.com>
2023-07-27 15:21:20 -0300

    Ajustando responsive mobile RegisterForm

--- src/components/form/RegisterForm.tsx
+++ src/components/form/RegisterForm.tsx
@@ -65,7 +65,7 @@ export const RegisterForm = () => {
 
   return (
     <form
-      className="flex flex-col bg-white rounded-xl h-1/2 md:w-[500px] w-full md:p-16 px-2 py-10 text-dark_grey"
+      className="flex flex-col bg-white rounded-xl md:h-1/2 h-[300px] md:w-[500px] w-full md:p-16 px-5 text-dark_grey"
       onSubmit={handleRegister}
     >
       <h3 className="text-4xl font-bold">Create account</h3>
